c808c361ef0ef7bf7b058cd43ab82c62a5070486
nakrlove <nakrlove@gmail.com>
2025-08-25 00:25:38 +0900

    Update urls.py
    urls 정리함

--- jangoai/urls.py
+++ jangoai/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path,include
 from bbs import views
 from django.conf import settings
@@ -11,14 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('bbs/', include('bbs.urls')),
-    # path('bbs/',include('bbs.urls')),
-    # path('send_push/', Push.as_view(), name='send_push'), 
-=======
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    # bbs 앱의 URL을 루트("/")에 연결
-    path("", include("bbs.urls")),
->>>>>>> 7f26a98dffa9cf835af6ea79046f57e8a94510b8
+  
 ]
