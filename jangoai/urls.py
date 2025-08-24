32f221513d4b1bbeaf392428165d684ba66db94d
nakrlove <nakrlove@gmail.com>
2025-08-24 23:52:41 +0900

    git test

--- jangoai/urls.py
+++ jangoai/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path,include
 from bbs import views
 from django.conf import settings
@@ -7,18 +6,17 @@ from django.conf.urls.static import static
 # from bbs.views import Push
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('bbs/', include('bbs.urls')),
-    # path('bbs/',include('bbs.urls')),
-    # path('send_push/', Push.as_view(), name='send_push'), 
-=======
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.index, name='index'),
+#     path('bbs/', include('bbs.urls')),
+#     # path('bbs/',include('bbs.urls')),
+#     # path('send_push/', Push.as_view(), name='send_push'), 
+# =======
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # bbs 앱의 URL을 루트("/")에 연결
     path("", include("bbs.urls")),
->>>>>>> 7f26a98dffa9cf835af6ea79046f57e8a94510b8
 ]
